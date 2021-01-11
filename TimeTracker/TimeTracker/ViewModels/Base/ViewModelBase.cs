730028074068fb3349133c09be6eb350a27efe63
Aaron Ramos <aj.ramos2794@gmail.com>
2021-01-10 17:31:02 -0800

    Template

---
+++ TimeTracker/TimeTracker/ViewModels/Base/ViewModelBase.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Xamarin.Forms;
+
+namespace TimeTracker.ViewModels.Base
+{
+    public class ViewModelBase: BindableObject
+    {
+        string _title;
+        public string Title
+        {
+            get => _title;
+            set => SetProperty(ref _title, value);
+        }
+
+        bool _isLoading;
+
+        public bool IsLoading
+        {
+            get => _isLoading;
+            set => SetProperty(ref _isLoading, value);
+        }
+
+        //Method to update properties
+        protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName]string propertyName = null)
+        {
+            if(EqualityComparer<T>.Default.Equals(storage, value))
+            {
+                return false;
+            }
+
+            storage = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+    }
+}
---
+++ TimeTracker/TimeTracker/ViewModels/Base/ViewModelLocator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TinyIoC;
+using Xamarin.Forms;
+
+namespace TimeTracker.ViewModels.Base
+{
+    public class ViewModelLocator
+    {
+        static TinyIoCContainer _container; //init ioc container
+        static Dictionary<Type, Type> _viewLookup; //to map our views to our viewmodels
+        
+        static ViewModelLocator()
+        {
+            _container = new TinyIoCContainer();
+            _viewLookup = new Dictionary<Type, Type>();
+
+            //Register pages and pages models
+
+            //Register services (services are registered as Singletons by default)
+        }
+
+        public static T Resolve<T>() where T : class //T probably a ViewModel
+        {
+            return _container.Resolve<T>();
+        }
+
+        //create a page for specific view model type
+        public static Page CreatePageFor(Type viewModelType)
+        {
+            var pageType = _viewLookup[viewModelType];
+            var page = (Page)Activator.CreateInstance(pageType);
+            var viewModel = _container.Resolve(viewModelType);
+
+            page.BindingContext = viewModel;
+
+            return page;
+        }
+
+
+    }
+}
